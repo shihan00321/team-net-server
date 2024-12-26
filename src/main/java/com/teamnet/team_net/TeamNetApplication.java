e1d88ac6a5142e5af67f1ab50cd40b575c306d58
shihan00321 <shihan001@naver.com>
2024-12-26 20:36:08 +0900

    :tada: Project Init

---
+++ src/main/java/com/teamnet/team_net/TeamNetApplication.java
@@ -0,0 +1,13 @@
+package com.teamnet.team_net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamnet/team_net/TeamNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamnet.team_net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
