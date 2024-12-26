a541f99e5676f09fc6872f64dac159bfacbbda79
shihan00321 <shihan001@naver.com>
2024-12-26 21:29:06 +0900

    :sparkles: 멤버 엔티티 구현

---
+++ src/main/java/com/teamnet/team_net/domain/member/entity/Member.java
@@ -0,0 +1,22 @@
+package com.teamnet.team_net.domain.member.entity;
+
+import com.teamnet.team_net.domain.member.enums.DeletionStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String nickname;
+    private String email;
+    private String profileImage;
+    @Enumerated(EnumType.STRING)
+    private DeletionStatus status;
+}
