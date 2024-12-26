16542fbe33bbfb107c302385c2399c41dd0e5a7a
shihan00321 <shihan001@naver.com>
2024-12-26 22:22:24 +0900

    :sparkles: 댓글 엔티티 구현

---
+++ src/main/java/com/teamnet/team_net/domain/comment/entity/Comment.java
@@ -0,0 +1,32 @@
+package com.teamnet.team_net.domain.comment.entity;
+
+import com.teamnet.team_net.domain.post.entity.Post;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> children = new ArrayList<>();
+}
