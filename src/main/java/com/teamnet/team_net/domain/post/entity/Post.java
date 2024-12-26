8ae83ff772dffca662da97f2c7e18f87f7f07e5b
shihan00321 <shihan001@naver.com>
2024-12-26 21:38:20 +0900

    :sparkles: 게시글 엔티티 구현

---
+++ src/main/java/com/teamnet/team_net/domain/post/entity/Post.java
@@ -0,0 +1,23 @@
+package com.teamnet.team_net.domain.post.entity;
+
+import com.teamnet.team_net.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String title;
+    private String content;
+}
