ab1c8551228fe6d087328fdd9e545e56ba3686b7
shihan00321 <shihan001@naver.com>
2024-12-29 17:40:57 +0900

    :sparkles: Feat: 게시글 전체 조회 기능 컨트롤러 테스트 구현

--- src/test/java/com/teamnet/team_net/domain/post/controller/PostControllerTest.java
+++ src/test/java/com/teamnet/team_net/domain/post/controller/PostControllerTest.java
@@ -10,6 +10,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -41,5 +47,29 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.title").value("테스트 제목"))
                 .andExpect(jsonPath("$.content").value("테스트 내용"))
                 .andDo(print());  // 테스트 결과를 콘솔에 출력
+    }
+
+    @Test
+    @DisplayName("게시글 모두 조회 테스트")
+    void 게시글_모두_조회() throws Exception {
+        List<PostResponse.PostResponseDto> posts = new ArrayList<>();
+        IntStream.rangeClosed(1, 5).forEach(i -> posts.add(
+                PostResponse.PostResponseDto.builder()
+                        .id((long) i)
+                        .title("테스트 제목" + i)
+                        .content("테스트 내용" + i)
+                        .build())
+        );
+
+        when(postService.findAll()).thenReturn(posts);
+
+        mvc.perform(get("/api/posts")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(5)))
+                .andExpect(jsonPath("$[1].id").value(2L))
+                .andExpect(jsonPath("$[1].title").value("테스트 제목2"))
+                .andExpect(jsonPath("$[1].content").value("테스트 내용2"))
+                .andDo(print());
     }
 }
