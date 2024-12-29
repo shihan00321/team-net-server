dc628c60455aef6bd784635532da9f8bcc436c33
shihan00321 <shihan001@naver.com>
2024-12-29 17:41:03 +0900

    :sparkles: Feat: 게시글 전체 조회 기능 서비스 테스트 구현

--- src/test/java/com/teamnet/team_net/domain/post/service/PostServiceTest.java
+++ src/test/java/com/teamnet/team_net/domain/post/service/PostServiceTest.java
@@ -3,11 +3,15 @@ package com.teamnet.team_net.domain.post.service;
 import com.teamnet.team_net.domain.post.dto.PostResponse;
 import com.teamnet.team_net.domain.post.entity.Post;
 import com.teamnet.team_net.domain.post.repository.PostRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -49,5 +53,23 @@ class PostServiceTest {
         // when & then
         assertThrows(IllegalStateException.class,
                 () -> postService.findOne(nonExistentId));
+    }
+
+    @Test
+    @DisplayName("게시글 모두 조회")
+    void 게시글_모두_조회() {
+        List<Post> posts = IntStream.range(1, 6)
+                .mapToObj(i -> Post.builder()
+                        .title("테스트 제목" + i)
+                        .content("테스트 내용" + i)
+                        .build())
+                .toList();
+        postRepository.saveAll(posts);
+
+        List<PostResponse.PostResponseDto> findAll = postService.findAll();
+        Assertions.assertThat(findAll.size()).isEqualTo(5);
+        Assertions.assertThat(findAll.get(2).getId()).isEqualTo(3);
+        Assertions.assertThat(findAll.get(2).getTitle()).isEqualTo("테스트 제목" + 3);
+        Assertions.assertThat(findAll.get(2).getContent()).isEqualTo("테스트 내용" + 3);
     }
 }
