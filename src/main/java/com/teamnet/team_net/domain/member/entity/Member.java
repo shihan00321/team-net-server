521fdc4d7cbf9d1675941dbd374e516d8f6752d4
shihan00321 <shihan001@naver.com>
2025-01-01 00:05:50 +0900

    :sparkles: Feat: Member 도메인 요구사항 추가

--- src/main/java/com/teamnet/team_net/domain/member/entity/Member.java
+++ src/main/java/com/teamnet/team_net/domain/member/entity/Member.java
@@ -1,7 +1,8 @@
 package com.teamnet.team_net.domain.member.entity;
 
-import com.teamnet.team_net.global.common.entity.BaseTimeEntity;
 import com.teamnet.team_net.domain.member.enums.DeletionStatus;
+import com.teamnet.team_net.domain.member.enums.Role;
+import com.teamnet.team_net.global.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -20,4 +21,17 @@ public class Member extends BaseTimeEntity {
     private String profileImage;
     @Enumerated(EnumType.STRING)
     private DeletionStatus status;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public Member update(String name, String email) {
+        this.name = name;
+        this.email = email;
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
