fe5dbfa7b36739220f86bdb0390893c049a3fad0
shihan00321 <shihan001@naver.com>
2025-01-01 00:06:45 +0900

    :sparkles: Feat: 세션 저장소의 사용자 정보를 가져오기 위한 애노테이션, ArgumentResolver 구현

---
+++ src/main/java/com/teamnet/team_net/global/config/auth/LoginMemberArgumentResolver.java
@@ -0,0 +1,29 @@
+package com.teamnet.team_net.global.config.auth;
+
+import com.teamnet.team_net.global.config.auth.dto.SessionMember;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginMemberAnnotation = parameter.getParameterAnnotation(LoginMember.class) != null;
+        boolean isMemberClass = SessionMember.class.equals(parameter.getParameterType());
+        return isLoginMemberAnnotation && isMemberClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("member");
+    }
+}
