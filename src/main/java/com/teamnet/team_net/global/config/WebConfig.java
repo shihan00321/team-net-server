42dedc13ce5f993662e0445d70b79ef02fea378c
shihan00321 <shihan001@naver.com>
2025-01-01 00:07:18 +0900

    :sparkles: Feat: 세션 저장소의 사용자 정보를 가져오기 위한 애노테이션, ArgumentResolver 추가

---
+++ src/main/java/com/teamnet/team_net/global/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.teamnet.team_net.global.config;
+
+import com.teamnet.team_net.global.config.auth.LoginMemberArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
+    }
+}
