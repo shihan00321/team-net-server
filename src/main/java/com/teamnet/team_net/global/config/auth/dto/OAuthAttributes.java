fd8126ca2da9a1c68a1745e30b82a6f8568fe100
shihan00321 <shihan001@naver.com>
2025-01-01 00:04:55 +0900

    :sparkles: Feat: 소셜 로그인 응답 DTO 생성

---
+++ src/main/java/com/teamnet/team_net/global/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,53 @@
+package com.teamnet.team_net.global.config.auth.dto;
+
+import com.teamnet.team_net.domain.member.entity.Member;
+import com.teamnet.team_net.domain.member.enums.DeletionStatus;
+import com.teamnet.team_net.domain.member.enums.Role;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@Builder
+public class OAuthAttributes {
+
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .name(name)
+                .email(email)
+                .role(Role.GUEST)
+                .status(DeletionStatus.NOT_DELETE)
+                .build();
+    }
+}
