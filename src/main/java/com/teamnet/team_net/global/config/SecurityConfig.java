fd5e9dc8ca2c455a4e0cbb2101f6bcff4303a8b8
shihan00321 <shihan001@naver.com>
2024-12-31 23:39:45 +0900

    :wrench: Feat: SpringSecurity 설정 추가

--- src/main/java/com/teamnet/team_net/global/config/SecurityConfig.java
+++ src/main/java/com/teamnet/team_net/global/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.teamnet.team_net.global.config;
 
+import com.teamnet.team_net.global.config.auth.CustomOAuth2UserService;
+import com.teamnet.team_net.global.config.auth.OAuth2LoginSuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,9 +12,13 @@ import org.springframework.security.config.annotation.web.configurers.CsrfConfig
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOauth2UserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,12 +26,14 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(HttpBasicConfigurer::disable);
 
-        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Login((oauth2) -> oauth2
+                .userInfoEndpoint((userInfoEndpointConfig) ->
+                        userInfoEndpointConfig.userService(customOauth2UserService))
+                .successHandler(oAuth2LoginSuccessHandler));
 
-        http.authorizeHttpRequests((auth) ->
-                auth
-                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
-                        .anyRequest().authenticated());
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+                .anyRequest().authenticated());
 
         return http.build();
     }
