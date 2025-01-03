c5a42899f7182ab2f725430b00d04e913c3857ab
shihan00321 <shihan001@naver.com>
2025-01-03 22:06:14 +0900

    :recycle: Refactor: @SessionMember Id 필드 추가

--- src/main/java/com/teamnet/team_net/global/config/auth/dto/SessionMember.java
+++ src/main/java/com/teamnet/team_net/global/config/auth/dto/SessionMember.java
@@ -7,10 +7,12 @@ import java.io.Serializable;
 
 @Getter
 public class SessionMember implements Serializable {
+    private Long id;
     private String name;
     private String email;
 
     public SessionMember(Member member) {
+        this.id = member.getId();
         this.name = member.getName();
         this.email = member.getEmail();
     }
