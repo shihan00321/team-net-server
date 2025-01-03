fbb9aaf441bb1b4e7ca067c8b7699775dc2e0e70
shihan00321 <shihan001@naver.com>
2025-01-03 22:06:50 +0900

    :bug: Chore: 인증 객체 변경을 위한 Config 재설정

--- src/main/java/com/teamnet/team_net/global/config/SecurityConfig.java
+++ src/main/java/com/teamnet/team_net/global/config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.teamnet.team_net.global.config;
 
+import com.teamnet.team_net.domain.member.enums.Role;
 import com.teamnet.team_net.global.config.auth.CustomOAuth2UserService;
 import com.teamnet.team_net.global.config.auth.OAuth2LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 @RequiredArgsConstructor
 @Configuration
@@ -32,9 +37,15 @@ public class SecurityConfig {
                 .successHandler(oAuth2LoginSuccessHandler));
 
         http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/api/members/test").hasAuthority(Role.USER.getKey())
                 .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
                 .anyRequest().authenticated());
 
         return http.build();
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
     }
 }
---
+++ src/main/java/com/teamnet/team_net/global/config/SessionConfig.java
@@ -0,0 +1,9 @@
+package com.teamnet.team_net.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@Configuration
+@EnableJdbcHttpSession
+public class SessionConfig {
+}
