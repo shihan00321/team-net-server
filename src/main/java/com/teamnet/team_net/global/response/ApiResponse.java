fff5fcf48e89389b2581d820ca18dcc154e7a3fd
shihan00321 <shihan001@naver.com>
2025-01-08 21:03:41 +0900

    :recycle: Refactor: 로직 실패 시 ApiResponse.result 반환되지 않도록 변경

--- src/main/java/com/teamnet/team_net/global/response/ApiResponse.java
+++ src/main/java/com/teamnet/team_net/global/response/ApiResponse.java
@@ -1,28 +1,27 @@
 package com.teamnet.team_net.global.response;
 
-import com.teamnet.team_net.global.response.code.BaseCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.teamnet.team_net.global.response.code.status.SuccessStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class ApiResponse<T> {
 
     private final Boolean isSuccess;
     private final String code;
     private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final T result;
 
     public static <T> ApiResponse<T> onSuccess(T result) {
         return new ApiResponse<>(true, SuccessStatus._OK.getCode(), SuccessStatus._OK.getMessage(), result);
     }
 
-    public static <T> ApiResponse<T> onFailure(String code, String message, T data) {
-        return new ApiResponse<>(false, code, message, data);
-    }
-
-    public static <T> ApiResponse<T> of(BaseCode code, T result){
-        return new ApiResponse<>(true, code.getReasonHttpStatus().getCode() , code.getReasonHttpStatus().getMessage(), result);
+    public static ApiResponse<Void> onFailure(String code, String message) {
+        return new ApiResponse<>(false, code, message, null);
     }
 }
