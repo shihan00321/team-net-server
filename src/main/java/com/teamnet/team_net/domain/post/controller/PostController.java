660d3fa184680e8266d98b917cf7696865bef33d
shihan00321 <shihan001@naver.com>
2025-01-08 20:08:40 +0900

    :recycle: Refactor: PostController 공통 응답 처리

--- src/main/java/com/teamnet/team_net/domain/post/controller/PostController.java
+++ src/main/java/com/teamnet/team_net/domain/post/controller/PostController.java
@@ -4,6 +4,7 @@ import com.teamnet.team_net.domain.post.dto.PostResponse;
 import com.teamnet.team_net.domain.post.service.PostService;
 import com.teamnet.team_net.global.config.auth.LoginMember;
 import com.teamnet.team_net.global.config.auth.dto.SessionMember;
+import com.teamnet.team_net.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,27 +21,27 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping
-    public ResponseEntity<List<PostResponse.PostResponseDto>> findAll() {
-        return new ResponseEntity<>(postService.findAll(), HttpStatus.OK);
+    public ApiResponse<List<PostResponse.PostResponseDto>> findAll() {
+        return ApiResponse.onSuccess(postService.findAll());
     }
 
     @GetMapping("/{postId}")
-    public ResponseEntity<PostResponse.PostResponseDto> findOne(@PathVariable("postId") Long postId) {
-        return new ResponseEntity<>(postService.findOne(postId), HttpStatus.OK);
+    public ApiResponse<PostResponse.PostResponseDto> findOne(@PathVariable("postId") Long postId) {
+        return ApiResponse.onSuccess(postService.findOne(postId));
     }
 
     @PostMapping
-    public ResponseEntity<Long> save(@LoginMember SessionMember sessionMember, @Valid @RequestBody PostRequest.PostSaveDto postSaveDto) {
-        return ResponseEntity.ok(postService.save(sessionMember.getId(), postSaveDto));
+    public ApiResponse<Long> save(@LoginMember SessionMember sessionMember, @Valid @RequestBody PostRequest.PostSaveDto postSaveDto) {
+        return ApiResponse.onSuccess(postService.save(sessionMember.getId(), postSaveDto));
     }
 
     @PatchMapping("/{postId}")
-    public ResponseEntity<Long> update(@LoginMember SessionMember sessionMember, @PathVariable("postId") Long postId, @Valid @RequestBody PostRequest.PostUpdateDto postUpdateDto) {
-        return ResponseEntity.ok(postService.update(sessionMember.getId(), postId, postUpdateDto));
+    public ApiResponse<Long> update(@LoginMember SessionMember sessionMember, @PathVariable("postId") Long postId, @Valid @RequestBody PostRequest.PostUpdateDto postUpdateDto) {
+        return ApiResponse.onSuccess(postService.update(sessionMember.getId(), postId, postUpdateDto));
     }
 
     @DeleteMapping("/{postId}")
-    public ResponseEntity<Long> delete(@LoginMember SessionMember sessionMember, @PathVariable("postId") Long postId) {
-        return ResponseEntity.ok(postService.delete(sessionMember.getId(), postId));
+    public ApiResponse<Long> delete(@LoginMember SessionMember sessionMember, @PathVariable("postId") Long postId) {
+        return ApiResponse.onSuccess(postService.delete(sessionMember.getId(), postId));
     }
 }
