d928e7da8409bfe6033c2417fdd4f84fc26ba767
shihan00321 <shihan001@naver.com>
2025-01-08 21:16:05 +0900

    :sparkles: Feat: 컨트롤러 하위에서 발생하는 모든 RuntimeException 공통처리

--- src/main/java/com/teamnet/team_net/global/exception/ExceptionAdvice.java
+++ src/main/java/com/teamnet/team_net/global/exception/ExceptionAdvice.java
@@ -2,6 +2,7 @@ package com.teamnet.team_net.global.exception;
 
 import com.teamnet.team_net.global.response.ApiResponse;
 import com.teamnet.team_net.global.response.code.ErrorReasonDTO;
+import com.teamnet.team_net.global.response.code.status.ErrorStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,6 +13,14 @@ public class ExceptionAdvice {
     @ExceptionHandler(value = GeneralException.class)
     public ResponseEntity<ApiResponse<Void>> onThrowException(GeneralException generalException) {
         ErrorReasonDTO e = generalException.getErrorReasonHttpStatus();
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(ApiResponse.onFailure(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<ApiResponse<Void>> onThrowRuntimeException() {
+        ErrorReasonDTO e = ErrorStatus._INTERNAL_SERVER_ERROR.getReasonHttpStatus();
         return ResponseEntity
                 .status(e.getHttpStatus())
                 .body(ApiResponse.onFailure(e.getCode(), e.getMessage()));
