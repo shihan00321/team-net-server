fd6424843766664bbbe37aff8745150788ad93f0
shihan00321 <shihan001@naver.com>
2025-01-09 19:20:47 +0900

    :sparkles: Feat: 스프링이 제공하는 404 예외 대신 공통 예외 응답하도록 변경

---
+++ src/main/java/com/teamnet/team_net/global/exception/BaseExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.teamnet.team_net.global.exception;
+
+import com.teamnet.team_net.global.response.ApiResponse;
+import com.teamnet.team_net.global.response.code.ErrorReasonDTO;
+import com.teamnet.team_net.global.response.code.status.ErrorStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+
+public abstract class BaseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // 스프링이 제공하는 404 예외
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorReasonDTO e = ErrorStatus._NOT_FOUND.getReasonHttpStatus();
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(ApiResponse.onFailure(e.getCode(), e.getMessage(), null));
+    }
+
+}
