0f7da1ab1cb23df9b13980b303cb5249bdb778e3
shihan00321 <shihan001@naver.com>
2025-01-13 21:37:55 +0900

    :sparkles: Feat: SSE 알림 이벤트를 관리할 EmitterRepository 추가

---
+++ src/main/java/com/teamnet/team_net/domain/sse/EmitterRepository.java
@@ -0,0 +1,28 @@
+package com.teamnet.team_net.domain.sse;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RequiredArgsConstructor
+@Repository
+public class EmitterRepository {
+
+    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    public SseEmitter save(Long emitterId, SseEmitter sseEmitter) {
+        emitters.put(emitterId, sseEmitter);
+        return sseEmitter;
+    }
+
+    public void deleteById(Long emitterId) {
+        emitters.remove(emitterId);
+    }
+
+    public SseEmitter get(Long emitterId) {
+        return emitters.get(emitterId);
+    }
+}
