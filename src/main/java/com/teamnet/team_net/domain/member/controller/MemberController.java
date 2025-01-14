71e40d40d354c9986cb38e073b9736de897e0650
shihan00321 <shihan001@naver.com>
2025-01-14 22:07:52 +0900

    :sparkles: Feat: 멤버 알림 조회 컨트롤러 구현

--- src/main/java/com/teamnet/team_net/domain/member/controller/MemberController.java
+++ src/main/java/com/teamnet/team_net/domain/member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.teamnet.team_net.domain.member.controller;
 
 import com.teamnet.team_net.domain.member.service.MemberService;
+import com.teamnet.team_net.domain.notification.dto.NotificationResponse;
+import com.teamnet.team_net.domain.notification.service.NotificationService;
 import com.teamnet.team_net.global.config.auth.LoginMember;
 import com.teamnet.team_net.global.config.auth.dto.SessionMember;
 import com.teamnet.team_net.global.response.ApiResponse;
@@ -8,11 +10,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+    private final NotificationService notificationService;
 
     @PostMapping("/additional")
     public ApiResponse<Long> saveAdditionalInfo(
@@ -27,5 +31,15 @@ public class MemberController {
             @LoginMember SessionMember sessionMember,
             HttpServletRequest request, HttpServletResponse response) {
         return ApiResponse.onSuccess(memberService.saveAdditionalMemberInfo(request, response, memberInfoDto, sessionMember.getId()));
+    }
+
+    @GetMapping("/alarm")
+    public ApiResponse<List<NotificationResponse.NotificationResponseDto>> alarm(@LoginMember SessionMember sessionMember) {
+        return ApiResponse.onSuccess(memberService.findNotificationList(sessionMember.getId()));
+    }
+
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@LoginMember SessionMember sessionMember) {
+        return notificationService.subscribe(sessionMember.getId());
     }
 }
