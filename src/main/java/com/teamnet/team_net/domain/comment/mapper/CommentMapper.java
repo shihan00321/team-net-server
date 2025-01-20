714de3ecc0b8c855f4951747d193f9b091c1c6f5
shihan00321 <shihan001@naver.com>
2025-01-20 18:25:16 +0900

    :recycle: Refactor: 요청 DTO를 엔티티로 변환하는 로직 Mapper로 이동

--- src/main/java/com/teamnet/team_net/domain/comment/mapper/CommentMapper.java
+++ src/main/java/com/teamnet/team_net/domain/comment/mapper/CommentMapper.java
@@ -1,15 +1,36 @@
 package com.teamnet.team_net.domain.comment.mapper;
 
+import com.teamnet.team_net.domain.comment.controller.CommentRequest;
+import com.teamnet.team_net.domain.comment.controller.CommentRequest.CreateCommentDto;
 import com.teamnet.team_net.domain.comment.dto.CommentResponse;
 import com.teamnet.team_net.domain.comment.entity.Comment;
+import com.teamnet.team_net.domain.post.entity.Post;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public abstract class CommentMapper {
+
+    public static Comment toComment(CreateCommentDto request, Post post, Comment parent) {
+        return Comment.builder()
+                .post(post)
+                .parent(parent)
+                .content(request.getContent())
+                .build();
+    }
+
+    public static CommentResponse.CommentResponseDTO toCommentResponseDTO(Comment comment) {
+        return CommentResponse.CommentResponseDTO.builder()
+                .commentId(comment.getId())
+                .parentId(comment.getParent() != null ? comment.getParent().getId() : null)
+                .content(comment.getContent())
+                .createdAt(comment.getCreatedAt())
+                .childrenComment(new ArrayList<>())
+                .build();
+    }
+
     public static List<CommentResponse.CommentResponseDTO> toCommentListResponseDTO(List<Comment> comments) {
         // 최상위 댓글을 담을 리스트
         List<CommentResponse.CommentResponseDTO> topLevelComments = new ArrayList<>();
@@ -22,13 +43,7 @@ public abstract class CommentMapper {
             //computeIfAbsent: Map에 키가 없으면 생성하고 저장, 있으면 기존 값을 반환.
             CommentResponse.CommentResponseDTO currentDto = commentMap.computeIfAbsent(
                     comment.getId(),
-                    id -> CommentResponse.CommentResponseDTO.builder()
-                            .commentId(comment.getId())
-                            .parentId(comment.getParent() != null ? comment.getParent().getId() : null)
-                            .content(comment.getContent())
-                            .createdAt(comment.getCreatedAt())
-                            .childrenComment(new ArrayList<>())
-                            .build()
+                    id -> toCommentResponseDTO(comment)
             );
 
             // 부모 댓글이 없으면 최상위 댓글 리스트에 추가
