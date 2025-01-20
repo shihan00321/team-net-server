356141fb9f310f7d36923a1e79873bd227766548
shihan00321 <shihan001@naver.com>
2025-01-20 18:20:48 +0900

    :recycle: Refactor: 자주 사용되는 엔티티 조회 로직 스프링 빈으로 따로 만들어 구분

---
+++ src/main/java/com/teamnet/team_net/global/utils/checker/EntityChecker.java
@@ -0,0 +1,54 @@
+package com.teamnet.team_net.global.utils.checker;
+
+import com.teamnet.team_net.domain.comment.entity.Comment;
+import com.teamnet.team_net.domain.comment.repository.CommentRepository;
+import com.teamnet.team_net.domain.member.entity.Member;
+import com.teamnet.team_net.domain.member.repository.MemberRepository;
+import com.teamnet.team_net.domain.post.entity.Post;
+import com.teamnet.team_net.domain.post.repository.PostRepository;
+import com.teamnet.team_net.domain.teammember.entity.TeamMember;
+import com.teamnet.team_net.domain.teammember.repository.TeamMemberRepository;
+import com.teamnet.team_net.global.exception.handler.CommentHandler;
+import com.teamnet.team_net.global.exception.handler.MemberHandler;
+import com.teamnet.team_net.global.exception.handler.PostHandler;
+import com.teamnet.team_net.global.exception.handler.TeamHandler;
+import com.teamnet.team_net.global.response.code.status.ErrorStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+public class EntityChecker {
+
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final TeamMemberRepository teamMemberRepository;
+
+    public Post findPostById(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new PostHandler(ErrorStatus.POST_NOT_FOUND));
+    }
+
+    public Comment findCommentById(Long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new CommentHandler(ErrorStatus.COMMENT_NOT_FOUND));
+    }
+
+    public Comment findParentCommentIfExists(Long parentId) {
+        if (parentId == null) {
+            return null;
+        }
+        return findCommentById(parentId);
+    }
+
+    public Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+    }
+
+    public TeamMember findByMemberIdAndTeamId(Long memberId, Long teamId) {
+        return teamMemberRepository.findByMemberIdAndTeamId(memberId, teamId)
+                .orElseThrow(() -> new TeamHandler(ErrorStatus.TEAM_MEMBER_NOT_FOUND));
+    }
+}
+
