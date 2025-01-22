ca8f8c72e6997db13b12e35c818d7fc97e84d99a
shihan00321 <shihan001@naver.com>
2025-01-22 19:50:55 +0900

    :wrench: Feat: Spring Data pageSerializationMode configuration 추가

--- src/main/java/com/teamnet/team_net/global/config/WebConfig.java
+++ src/main/java/com/teamnet/team_net/global/config/WebConfig.java
@@ -3,11 +3,13 @@ package com.teamnet.team_net.global.config;
 import com.teamnet.team_net.global.config.auth.LoginMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
