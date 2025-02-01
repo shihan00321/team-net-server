def21b75a5afb78e46408642607b38b7fbf67cc4
shihan00321 <shihan001@naver.com>
2025-02-01 15:41:33 +0900

    :memo: Docs: 게시글 컨트롤러 API 문서화

--- src/test/java/com/teamnet/team_net/docs/post/PostControllerDocsTest.java
+++ src/test/java/com/teamnet/team_net/docs/post/PostControllerDocsTest.java
@@ -26,14 +26,14 @@ import java.util.stream.IntStream;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -67,12 +67,29 @@ public class PostControllerDocsTest extends RestDocsSupport {
             when(postService.findOne(TEST_POST_ID)).thenReturn(responseDto);
 
             mvc.perform(get(BASE_URL + "/{postId}", TEST_TEAM_ID, TEST_POST_ID))
+                    .andDo(print())
                     .andExpectAll(
                             status().isOk(),
                             jsonPath("$.isSuccess").value(true),
                             jsonPath("$.result.title").value(TEST_TITLE),
                             jsonPath("$.result.content").value(TEST_CONTENT)
-                    ).andDo(print());
+                    )
+                    .andDo(document("post-find",
+                            preprocessRequest(prettyPrint()),
+                            preprocessResponse(prettyPrint()),
+                            responseFields(
+                                    fieldWithPath("isSuccess").type(JsonFieldType.BOOLEAN).description("성공 여부"),
+                                    fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+                                    fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메세지"),
+                                    fieldWithPath("result.id").type(JsonFieldType.NUMBER)
+                                            .description("게시글 ID"),
+                                    fieldWithPath("result.title").type(JsonFieldType.STRING)
+                                            .description("게시글 제목"),
+                                    fieldWithPath("result.content").type(JsonFieldType.STRING)
+                                            .description("게시글 내용"),
+                                    fieldWithPath("result.createdAt").type(JsonFieldType.STRING)
+                                            .description("게시글 작성 시간")
+                            )));
         }
 
         @Test
@@ -117,6 +134,8 @@ public class PostControllerDocsTest extends RestDocsSupport {
                             jsonPath("$.result.posts.page.number").value(0),
                             jsonPath("$.result.posts.page.totalPages").value(1))
                     .andDo(document("posts-find-all",
+                            preprocessRequest(prettyPrint()),
+                            preprocessResponse(prettyPrint()),
                             queryParameters(
                                     parameterWithName("keyword").description("검색 키워드"),
                                     parameterWithName("type").description("검색 타입 (TITLE, CONTENT, TITLE_CONTENT, AUTHOR)")
@@ -146,6 +165,130 @@ public class PostControllerDocsTest extends RestDocsSupport {
                             )
                     ));
         }
+    }
+
+    @Nested
+    @DisplayName("새로운 게시글을 등록한다.")
+    class savePostTest {
+        @Test
+        @DisplayName("신규 게시글을 성공적으로 등록한다.")
+        @WithMockUser(roles = "USER")
+        void savePost() throws Exception {
+            //given
+            PostRequest.PostSaveDTO request = createPostSaveDto(TEST_TITLE, TEST_CONTENT);
+            PostResponse.PostResponseDto responseDto = createPostResponseDto(TEST_POST_ID, TEST_TITLE, TEST_CONTENT);
+
+            //when & then
+            given(postService.save(eq(sessionMember.getId()), eq(TEST_TEAM_ID), any(PostServiceDTO.PostSaveServiceDTO.class)))
+                    .willReturn(responseDto);
+
+            mvc.perform(post(BASE_URL, TEST_TEAM_ID)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .session(mockHttpSession)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andDo(print())
+                    .andExpectAll(
+                            status().isOk(),
+                            jsonPath("$.isSuccess").value(true),
+                            jsonPath("$.result.title").value(responseDto.getTitle()),
+                            jsonPath("$.result.content").value(responseDto.getContent())
+                    )
+                    .andDo(document("post-create",
+                            preprocessRequest(prettyPrint()),
+                            preprocessResponse(prettyPrint()),
+                            requestFields(
+                                    fieldWithPath("title").type(JsonFieldType.STRING).description("게시글 제목"),
+                                    fieldWithPath("content").type(JsonFieldType.STRING).description("게시글 내용")
+                            ),
+                            responseFields(
+                                    fieldWithPath("isSuccess").type(JsonFieldType.BOOLEAN).description("성공 여부"),
+                                    fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+                                    fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메세지"),
+                                    fieldWithPath("result.id").type(JsonFieldType.NUMBER)
+                                            .description("게시글 ID"),
+                                    fieldWithPath("result.title").type(JsonFieldType.STRING)
+                                            .description("게시글 제목"),
+                                    fieldWithPath("result.content").type(JsonFieldType.STRING)
+                                            .description("게시글 내용"),
+                                    fieldWithPath("result.createdAt").type(JsonFieldType.STRING)
+                                            .description("게시글 작성 시간")
+                            )
+                    ));
+        }
+    }
+
+    @Nested
+    @DisplayName("게시글을 수정한다.")
+    class UpdatePostTest {
+        @Test
+        @DisplayName("게시글을 성공적으로 수정한다.")
+        @WithMockUser(roles = "USER")
+        void updatePost() throws Exception {
+            // given
+            PostResponse.PostResponseDto responseDto = createPostResponseDto(TEST_POST_ID, TEST_TITLE, TEST_CONTENT);
+            PostRequest.PostUpdateDTO updateDto = PostRequest.PostUpdateDTO.builder()
+                    .title("수정된 제목")
+                    .content("수정된 내용")
+                    .build();
+
+            //when & then
+            when(postService.update(
+                    eq(sessionMember.getId()),
+                    eq(TEST_POST_ID),
+                    any(PostServiceDTO.PostUpdateServiceDTO.class))
+            ).thenReturn(responseDto);
+
+            mvc.perform(patch(BASE_URL + "/{postId}", TEST_TEAM_ID, TEST_POST_ID)
+                            .session(mockHttpSession)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(updateDto)))
+                    .andDo(print())
+                    .andExpectAll(
+                            status().isOk(),
+                            jsonPath("$.result.title").value(responseDto.getTitle()),
+                            jsonPath("$.result.content").value(responseDto.getContent())
+                    ).andDo(document("post-update",
+                            preprocessRequest(prettyPrint()),
+                            preprocessResponse(prettyPrint()),
+                            requestFields(
+                                    fieldWithPath("title").type(JsonFieldType.STRING).description("수정된 게시글 제목"),
+                                    fieldWithPath("content").type(JsonFieldType.STRING).description("수정된 게시글 내용")
+                            ),
+                            responseFields(
+                                    fieldWithPath("isSuccess").type(JsonFieldType.BOOLEAN).description("성공 여부"),
+                                    fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+                                    fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메세지"),
+                                    fieldWithPath("result.id").type(JsonFieldType.NUMBER)
+                                            .description("게시글 ID"),
+                                    fieldWithPath("result.title").type(JsonFieldType.STRING)
+                                            .description("게시글 제목"),
+                                    fieldWithPath("result.content").type(JsonFieldType.STRING)
+                                            .description("게시글 내용"),
+                                    fieldWithPath("result.createdAt").type(JsonFieldType.STRING)
+                                            .description("게시글 작성 시간")
+                            )
+                    ));
+        }
+    }
+
+    @Test
+    @DisplayName("게시글을 삭제한다.")
+    @WithMockUser(roles = "USER")
+    void delete_test() throws Exception {
+        //when & then
+        doNothing().when(postService).delete(sessionMember.getId(), TEST_POST_ID);
+        mvc.perform(delete(BASE_URL + "/{postId}", TEST_TEAM_ID, TEST_POST_ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .session(mockHttpSession))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("post-delete",
+                        responseFields(
+                                fieldWithPath("isSuccess").description("성공 여부"),
+                                fieldWithPath("code").description("응답 코드"),
+                                fieldWithPath("message").description("응답 메세지")
+                        )
+                ));
     }
 
     private PostResponse.PostResponseDto createPostResponseDto(Long id, String title, String content) {
