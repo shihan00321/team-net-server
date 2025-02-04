cd4dc0f74dfe496ca29a76894220e83c5ced2e5e
shihan00321 <shihan001@naver.com>
2025-02-04 16:06:41 +0900

    :sparkles: Feat: CORS 설정 추가

--- src/main/java/com/teamnet/team_net/global/config/SecurityConfig.java
+++ src/main/java/com/teamnet/team_net/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.teamnet.team_net.domain.member.enums.Role;
 import com.teamnet.team_net.domain.member.service.SecurityContextManager;
 import com.teamnet.team_net.global.config.auth.CustomAccessDeniedHandler;
 import com.teamnet.team_net.global.config.auth.CustomOAuth2UserService;
+import com.teamnet.team_net.global.config.auth.OAuth2LoginFailureHandler;
 import com.teamnet.team_net.global.config.auth.OAuth2LoginSuccessHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,6 +20,13 @@ import org.springframework.security.config.annotation.web.configurers.HttpBasicC
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -28,18 +36,21 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOauth2UserService;
     private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(CsrfConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(HttpBasicConfigurer::disable);
+                .httpBasic(HttpBasicConfigurer::disable)
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(apiConfigurationSource()));
 
         http.oauth2Login((oauth2) -> oauth2
                         .userInfoEndpoint((userInfoEndpointConfig) ->
                                 userInfoEndpointConfig.userService(customOauth2UserService))
-                        .successHandler(oAuth2LoginSuccessHandler))
+                        .successHandler(oAuth2LoginSuccessHandler)
+                        .failureHandler(oAuth2LoginFailureHandler))
                 .exceptionHandling(handler -> handler
                         .accessDeniedHandler(customAccessDeniedHandler));
 
@@ -47,6 +58,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/members/additional").hasAuthority(Role.GUEST.getKey())
                 .requestMatchers("/api/posts/**", "/api/teams/**").hasAuthority(Role.USER.getKey())
                 .requestMatchers("/", "/oauth2/**", "/login/**", "/docs/**").permitAll()
+                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico").permitAll()
                 .anyRequest().authenticated());
 
         return http.build();
@@ -64,5 +76,20 @@ public class SecurityConfig {
             HttpServletResponse response
     ) {
         return new SecurityContextManager(securityContextRepository, request, response);
+    }
+
+    @Bean
+    CorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 서비스의 모든 경로에 대해서 판단
+        return source;
     }
 }
