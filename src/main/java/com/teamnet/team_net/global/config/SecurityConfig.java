515359d7b98610ee4641635d940278d2360c7e83
shihan00321 <shihan001@naver.com>
2025-02-06 19:54:52 +0900

    :wrench: Refactor: SecurityConfig 로그아웃 로직 추가

--- src/main/java/com/teamnet/team_net/global/config/SecurityConfig.java
+++ src/main/java/com/teamnet/team_net/global/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
@@ -26,7 +27,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -44,7 +44,14 @@ public class SecurityConfig {
         http.csrf(CsrfConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(HttpBasicConfigurer::disable)
-                .cors(corsConfigurer -> corsConfigurer.configurationSource(apiConfigurationSource()));
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(apiConfigurationSource()))
+                .logout(logout -> logout
+                        .logoutUrl("/api/logout")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            SecurityContextHolder.clearContext();
+                            request.getSession().invalidate();
+                        })
+                );
 
         http.oauth2Login((oauth2) -> oauth2
                         .userInfoEndpoint((userInfoEndpointConfig) ->
