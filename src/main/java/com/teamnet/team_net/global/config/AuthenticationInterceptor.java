df769ba3c3a1bb13170f59e14248d95ce6f01b35
shihan00321 <shihan001@naver.com>
2025-02-20 18:36:50 +0900

    :sparkles: Feat: 웹 소켓 통신 시 인증 객체 등록을 위한 인터셉터 구현

---
+++ src/main/java/com/teamnet/team_net/global/config/AuthenticationInterceptor.java
@@ -0,0 +1,28 @@
+package com.teamnet.team_net.global.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AuthenticationInterceptor implements ChannelInterceptor {
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+        if (accessor != null && StompCommand.CONNECT.equals(accessor.getCommand())) {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth != null) {
+                accessor.setUser(auth);
+            }
+        }
+        return message;
+    }
+}
