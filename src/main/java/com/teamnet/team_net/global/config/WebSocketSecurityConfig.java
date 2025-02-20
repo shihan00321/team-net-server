476e71d3ee4c86767be31607de6c4b15949fad18
shihan00321 <shihan001@naver.com>
2025-02-20 18:36:33 +0900

    :sparkles: Feat: 웹 소켓 통신 Security configuration 구현

---
+++ src/main/java/com/teamnet/team_net/global/config/WebSocketSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.teamnet.team_net.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+@Configuration
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry message) {
+        message
+                .nullDestMatcher().permitAll()
+                .simpDestMatchers("/ws-connect/**").authenticated()
+                .simpDestMatchers("/publish/**").authenticated()
+                .simpSubscribeDestMatchers("/subscribe/**").authenticated()
+                .anyMessage().denyAll();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
