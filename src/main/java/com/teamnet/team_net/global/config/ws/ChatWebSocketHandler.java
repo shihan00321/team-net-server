8c4ea5c5f2e24e40ff273cc790fa4da5d5d950ea
shihan00321 <shihan001@naver.com>
2025-02-22 16:45:17 +0900

    :sparkles: Feat : WebSocketSession 연결 관리를 위한 ChatWebSocketHandler 구현

---
+++ src/main/java/com/teamnet/team_net/global/config/ws/ChatWebSocketHandler.java
@@ -0,0 +1,32 @@
+package com.teamnet.team_net.global.config.ws;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        super.handleMessage(session, message);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        super.handleTransportError(session, exception);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+}
