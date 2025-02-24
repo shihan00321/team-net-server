06187cad38d08e2d3991e1dca6db9edbcaf61441
shihan00321 <shihan001@naver.com>
2025-02-24 23:42:18 +0900

    :recycle: Refactor : 채팅 메시지 사이즈 검증 애노테이션 추가

--- src/main/java/com/teamnet/team_net/domain/chat/controller/ChatRequest.java
+++ src/main/java/com/teamnet/team_net/domain/chat/controller/ChatRequest.java
@@ -1,6 +1,7 @@
 package com.teamnet.team_net.domain.chat.controller;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 public class ChatRequest {
@@ -11,6 +12,7 @@ public class ChatRequest {
     @AllArgsConstructor(access = AccessLevel.PROTECTED)
     public static class CreateMessageDTO {
         @NotBlank(message = "메시지는 비어있을 수 없습니다.")
+        @Size(max = 500, message = "메시지는 500자를 넘어갈 수 없습니다.")
         String message;
     }
 }
