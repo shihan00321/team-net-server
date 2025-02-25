9f190d8ef5b40c620a66d88f654feeafa91bedec
shihan00321 <shihan001@naver.com>
2025-02-25 21:19:58 +0900

    :fire: Refactor : Request DTO 빌더, 생성자 제거

--- src/main/java/com/teamnet/team_net/domain/chat/controller/ChatRequest.java
+++ src/main/java/com/teamnet/team_net/domain/chat/controller/ChatRequest.java
@@ -7,9 +7,6 @@ import lombok.*;
 public class ChatRequest {
 
     @Getter
-    @Builder
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor(access = AccessLevel.PROTECTED)
     public static class CreateMessageDTO {
         @NotBlank(message = "메시지는 비어있을 수 없습니다.")
         @Size(max = 500, message = "메시지는 500자를 넘어갈 수 없습니다.")
