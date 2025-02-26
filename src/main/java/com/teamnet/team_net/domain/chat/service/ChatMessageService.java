c1c168584cc92f6823cf0001b50b10704d935783
shihan00321 <shihan001@naver.com>
2025-02-26 16:54:59 +0900

    :coffin: Refactor : 불필요한 Import 제거

--- src/main/java/com/teamnet/team_net/domain/chat/service/ChatMessageService.java
+++ src/main/java/com/teamnet/team_net/domain/chat/service/ChatMessageService.java
@@ -2,12 +2,10 @@ package com.teamnet.team_net.domain.chat.service;
 
 import com.teamnet.team_net.domain.chat.service.dto.ChatResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.aop.support.AopUtils;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.user.SimpUser;
 import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @RequiredArgsConstructor
 @Service
@@ -16,14 +14,14 @@ public class ChatMessageService {
     private final ChatService chatService;
     private final SimpUserRegistry simpUserRegistry;
     private final SimpMessagingTemplate messagingTemplate;
+
     public void sendMessage(String senderId, Long memberId, Long teamId, String message) {
-        System.out.println("cms : " + TransactionSynchronizationManager.isActualTransactionActive());
         ChatResponse.ChatResponseDTO response = chatService.createMessage(memberId, teamId, message);
         for (SimpUser user : simpUserRegistry.getUsers()) {
             if (!senderId.equals(user.getPrincipal().getName())) {
                 messagingTemplate.convertAndSendToUser(
                         user.getName(),
-                        "/subscribe/topic/teams." + teamId,
+                        "/subscribe/teams." + teamId,
                         response
                 );
             }
