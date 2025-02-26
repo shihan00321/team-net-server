be3bbb63c130d6335e4b17b411ebaab9e9bac007
shihan00321 <shihan001@naver.com>
2025-02-26 16:55:51 +0900

    :sparkles: Feat : 웹소켓 보안 설정 추가

--- src/main/java/com/teamnet/team_net/global/config/WebSocketConfig.java
+++ src/main/java/com/teamnet/team_net/global/config/WebSocketConfig.java
@@ -1,18 +1,18 @@
 package com.teamnet.team_net.global.config;
 
 import com.teamnet.team_net.global.config.auth.LoginMemberMessagingArgumentResolver;
-import com.teamnet.team_net.global.config.ws.AuthenticationInterceptor;
 import com.teamnet.team_net.global.config.ws.ChatWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
-import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -24,20 +24,14 @@ import org.springframework.web.socket.server.support.HttpSessionHandshakeInterce
 
 import java.util.List;
 
-@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @RequiredArgsConstructor
 @Configuration
+@EnableWebSocketSecurity
 @EnableWebSocketMessageBroker  // 메시지 브로커 : 메시지 전송을 중개하는 역할
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final LoginMemberMessagingArgumentResolver loginMemberArgumentResolver;
-    private final AuthenticationInterceptor authenticationInterceptor;
     private final ChatWebSocketHandler chatWebSocketHandler;
-
-    @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
-    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -58,18 +52,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-connect")
                 .setAllowedOrigins("http://localhost:5173")
-                .addInterceptors(new HttpSessionHandshakeInterceptor())
-                .withSockJS();
+                .addInterceptors(httpSessionHandshakeInterceptor());
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginMemberArgumentResolver);
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(authenticationInterceptor);
     }
 
     @Override
@@ -87,5 +75,27 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 super.afterConnectionClosed(session, closeStatus);
             }
         });
+    }
+
+    @Bean
+    HttpSessionHandshakeInterceptor httpSessionHandshakeInterceptor() {
+        return new HttpSessionHandshakeInterceptor();
+    }
+
+    @Bean
+    public AuthorizationManager<Message<?>> messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder messages) {
+        return messages
+                .nullDestMatcher().authenticated() // 목적지가 없는 메시지 인증 필요
+                .simpTypeMatchers(SimpMessageType.CONNECT).authenticated() // WebSocket에서 클라이언트가 연결할 때 보내는 CONNECT 메시지에 대해 사용자가 인증된 상태여야만 연결을 허용
+                .simpDestMatchers("/publish/**").hasRole("USER") // 해당 경로로 보내는 메시지에 대해 USER 역할을 가진 사용자만 접근 가능하도록 설정
+                .simpSubscribeDestMatchers("/user/**", "/subscribe/**").hasRole("USER") // 해당 경로를 구독하려면 USER 역할을 가진 사용자
+                .anyMessage().denyAll() // 나머지 모두 거부
+                .build();
+    }
+
+    @Bean("csrfChannelInterceptor")
+    public ChannelInterceptor csrfChannelInterceptor() {
+        return new ChannelInterceptor() {
+        };
     }
 }
