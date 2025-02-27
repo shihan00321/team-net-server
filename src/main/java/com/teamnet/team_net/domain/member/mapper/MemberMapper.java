31d75d46a319f43085fdc620c34d82a56d21de28
shihan00321 <shihan001@naver.com>
2025-02-27 22:15:18 +0900

    :recycle: Refactor : MemberMapper MapStruct 적용

--- src/main/java/com/teamnet/team_net/domain/member/mapper/MemberMapper.java
+++ src/main/java/com/teamnet/team_net/domain/member/mapper/MemberMapper.java
@@ -2,12 +2,11 @@ package com.teamnet.team_net.domain.member.mapper;
 
 import com.teamnet.team_net.domain.member.service.dto.MemberResponse.UpdateMemberResponseDto;
 import com.teamnet.team_net.domain.member.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-public abstract class MemberMapper {
-    public static UpdateMemberResponseDto toUpdateMemberResponseDto(Member member) {
-        return UpdateMemberResponseDto.builder()
-                .updateMemberId(member.getId())
-                .nickname(member.getNickname())
-                .build();
-    }
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+    @Mapping(source = "id", target = "updateMemberId")
+    UpdateMemberResponseDto toUpdateMemberResponseDto(Member member);
 }
