eef8fddb3268359bae4f9938edef4155875ae432
shihan00321 <shihan001@naver.com>
2025-03-18 17:12:35 +0900

    :recycle: Refactor : Redis에서 세션 조회 후 메시지 전송하도록 변경

--- src/main/java/com/teamnet/team_net/domain/chat/service/ChatMessageConsumer.java
+++ src/main/java/com/teamnet/team_net/domain/chat/service/ChatMessageConsumer.java
@@ -3,17 +3,22 @@ package com.teamnet.team_net.domain.chat.service;
 import com.teamnet.team_net.domain.chat.service.dto.ChatResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.user.SimpUser;
 import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class ChatMessageConsumer {
 
     private final SimpMessagingTemplate messagingTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @KafkaListener(topicPattern = "chat-messages-.*", containerFactory = "kafkaListenerContainerFactory")
     public void consume(ConsumerRecord<String, ChatResponse.ChatResponseDTO> record) {
@@ -21,21 +26,22 @@ public class ChatMessageConsumer {
         Long teamId = Long.parseLong(topic.substring("chat-messages-".length()));
 
         ChatResponse.ChatResponseDTO message = record.value();
+        String senderId = record.key();
+        String redisKey = "team:sessions:" + teamId;
 
-        messagingTemplate.convertAndSend(
-                "/subscribe/teams." + teamId,
-                message
-        );
+        SetOperations<String, String> setOperations = redisTemplate.opsForSet();
+        Set<String> teamUsers = setOperations.members(redisKey);
+
+        if (teamUsers != null && !teamUsers.isEmpty()) {
+            for (String userName : teamUsers) {
+                if (!userName.equals(senderId)) {
+                    messagingTemplate.convertAndSendToUser(
+                            userName,
+                            "/subscribe/teams." + teamId,
+                            message
+                    );
+                }
+            }
+        }
     }
-
 }
-
-//        for (SimpUser user : simpUserRegistry.getUsers()) {
-//            if (!senderId.equals(user.getName())) {
-//                messagingTemplate.convertAndSendToUser(
-//                        user.getName(),
-//                        "/subscribe/teams." + teamId,
-//                        message
-//                );
-//            }
-//        }
